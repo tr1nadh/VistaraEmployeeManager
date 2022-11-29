cd575e7fd117d40a7b33f86a8db1ab87325b72aa
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-11-29 16:09:07 +0530

    Other files

--- src/main/java/com/example/vistaraemployeemanager/Main.java
+++ src/main/java/com/example/vistaraemployeemanager/Main.java
@@ -1,7 +1,13 @@
 package com.example.vistaraemployeemanager;
 
-public class Main {
-    public static void main(String[] args) {
+import com.example.vistaraemployeemanager.EmployeeManager.EmployeeManager;
+import com.example.vistaraemployeemanager.EmployeeManager.Database.EmployeeDB;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+    	var list = EmployeeManager.getAllEmployees();
+    	for (var emp : list) {
+    		System.out.println(emp);
+    	}
     }
 }
