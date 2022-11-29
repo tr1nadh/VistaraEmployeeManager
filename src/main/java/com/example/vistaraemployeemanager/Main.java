a9991443cb9b76badd2903bd2222ddf6c676ee86
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-11-29 16:22:16 +0530

    Main clean up

--- src/main/java/com/example/vistaraemployeemanager/Main.java
+++ src/main/java/com/example/vistaraemployeemanager/Main.java
@@ -1,8 +1,5 @@
 package com.example.vistaraemployeemanager;
 
-import com.example.vistaraemployeemanager.EmployeeManager.EmployeeManager;
-import com.example.vistaraemployeemanager.EmployeeManager.Database.EmployeeDB;
-
 public class Main {
     public static void main(String[] args) throws Exception {
     	
