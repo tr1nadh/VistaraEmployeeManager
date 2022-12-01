f3992abe4bed0a9fae29dd00d5f9a05b1335b964
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-01 08:00:58 +0530

    some

--- src/main/java/com/example/vistaraemployeemanager/EM/EmployeeManager.java
+++ src/main/java/com/example/vistaraemployeemanager/EM/EmployeeManager.java
@@ -1,6 +1,6 @@
 package com.example.vistaraemployeemanager.EM;
 
-import com.example.vistaraemployeemanager.EmployeeManager.Database.EmployeeDB;
+import com.example.vistaraemployeemanager.EmployeeManager.Database.EmployeeDao;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -8,50 +8,50 @@ import java.util.ArrayList;
 public class EmployeeManager {
 
     public static void add(Employee employee) throws Exception {
-        if (EmployeeDB.getAllottedIDs().size() > EmployeeIDGenerator.getIdLimit())
+        if (EmployeeDao.getAllottedIDs().size() > EmployeeIDGenerator.getIdLimit())
             throw new IllegalStateException("Employee limit reached!!!");
 
         employee.setId(generateId());
-        var response = EmployeeDB.addAsync(employee);
+        var response = EmployeeDao.addAsync(employee);
         if (!response.get()) System.out.println("Employee added: " + employee.getName());
         else System.out.println("Unable to add employee: " + employee.getName());
     }
 
     public static void remove(int employeeId) throws Exception {
-        if (!EmployeeDB.getAllottedIDs().contains(employeeId))
+        if (!EmployeeDao.getAllottedIDs().contains(employeeId))
             throw new IllegalStateException("Invalid ID");
 
-        var response = EmployeeDB.removeAsync(employeeId);
+        var response = EmployeeDao.removeAsync(employeeId);
         if (response.get() > 0) System.out.println("Employee with ID: " + employeeId + " is removed");
         else System.out.println("Unable to remove employee with ID: " + employeeId);
     }
 
     public static void update(int id, Employee employee) throws Exception {
-        if (!EmployeeDB.getAllottedIDs().contains(id))
+        if (!EmployeeDao.getAllottedIDs().contains(id))
             throw new IllegalStateException("Invalid ID");
 
-        var response = EmployeeDB.updateAsync(id, employee);
+        var response = EmployeeDao.updateAsync(id, employee);
         if (response.get() > 0) System.out.println("Changes saved of ID: " + id);
         else System.out.println("Unable to save changes of ID: " + id);
     }
 
     public static ArrayList<Employee> getAllEmployees() throws Exception {
-        var employees = EmployeeDB.getEmployeesAsync();
+        var employees = EmployeeDao.getEmployeesAsync();
         System.out.println("Successfully retrieved employees");
         return employees.get();
     }
 
     public static Employee getEmployee(int id) throws SQLException {
-        if (!EmployeeDB.getAllottedIDs().contains(id))
+        if (!EmployeeDao.getAllottedIDs().contains(id))
             throw new IllegalStateException("Invalid ID");
 
-        var employee = EmployeeDB.getEmployee(id);
+        var employee = EmployeeDao.getEmployee(id);
         System.out.println("Successfully retrieved employee with id: " + id);
         return employee;
     }
     private static int generateId() throws Exception {
         var id = EmployeeIDGenerator.generateID();
-        while (EmployeeDB.getAllottedIDsAsync().get().contains(id)) {
+        while (EmployeeDao.getAllottedIDsAsync().get().contains(id)) {
             id = EmployeeIDGenerator.generateID();
         }
 
