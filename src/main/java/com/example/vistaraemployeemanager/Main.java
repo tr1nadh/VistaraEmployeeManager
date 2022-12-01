2b1608206f9da19a8e8c74ce2b2fa7d205cea890
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-01 21:26:24 +0530

    Testing new JDBI3

--- src/main/java/com/example/vistaraemployeemanager/Main.java
+++ src/main/java/com/example/vistaraemployeemanager/Main.java
@@ -1,6 +1,24 @@
 package com.example.vistaraemployeemanager;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.FieldMapper;
+
+import com.example.vistaraemployeemanager.Dao.Datasource;
+import com.example.vistaraemployeemanager.Dao.EmployeeDBQueryManager;
+import com.example.vistaraemployeemanager.EM.Employee;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        var jdbi = Jdbi.create(Datasource.getDatasource());
+        jdbi.registerRowMapper(FieldMapper.factory(Employee.class));
+        var query = EmployeeDBQueryManager.getEmployee(4);
+        var result = (Optional<Employee>) jdbi.withHandle(handle -> {
+            var handleQuery = handle.createQuery(query);
+            return handleQuery.mapTo(Employee.class).findFirst();
+        });
+        System.out.println(result);
     }
 }
