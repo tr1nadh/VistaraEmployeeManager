fd7e2a45437421e2b72f4d1504125c957682da1a
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-02 10:14:33 +0530

    Removed Id check

--- src/main/java/com/example/vistaraemployeemanager/EM/EmployeeManager.java
+++ src/main/java/com/example/vistaraemployeemanager/EM/EmployeeManager.java
@@ -9,28 +9,18 @@ import java.util.Optional;
 public class EmployeeManager {
 
     public static void add(Employee employee) throws Exception {
-        if (EmployeeDao.getAllottedIDs().size() > EmployeeIDGenerator.getIdLimit())
-            throw new IllegalStateException("Employee limit reached!!!");
-
-        employee.setId(generateId());
         var response = EmployeeDao.add(employee);
         if (response > 0) System.out.println("Employee added: " + employee.getName());
         else System.out.println("Unable to add employee: " + employee.getName());
     }
 
     public static void remove(int employeeId) throws Exception {
-        if (!EmployeeDao.getAllottedIDs().contains(employeeId))
-            throw new IllegalStateException("Invalid ID");
-
         var response = EmployeeDao.removeAsync(employeeId);
         if (response.get() > 0) System.out.println("Employee with ID: " + employeeId + " is removed");
         else System.out.println("Unable to remove employee with ID: " + employeeId);
     }
 
     public static void update(int id, Employee employee) throws Exception {
-        if (!EmployeeDao.getAllottedIDs().contains(id))
-            throw new IllegalStateException("Invalid ID");
-
         var response = EmployeeDao.updateAsync(id, employee);
         if (response.get() > 0) System.out.println("Changes saved of ID: " + id);
         else System.out.println("Unable to save changes of ID: " + id);
@@ -43,26 +33,8 @@ public class EmployeeManager {
     }
 
     public static Optional<Employee> getEmployee(int id) throws SQLException {
-        if (!EmployeeDao.getAllottedIDs().contains(id))
-            throw new IllegalStateException("Invalid ID");
-
         var employee = EmployeeDao.getEmployee(id);
         System.out.println("Successfully retrieved employee with id: " + id);
         return employee;
-    }
-    private static int generateId() throws Exception {
-        var id = EmployeeIDGenerator.generateID();
-        while (EmployeeDao.getAllottedIDsAsync().get().contains(id)) {
-            id = EmployeeIDGenerator.generateID();
-        }
-
-        return id;
-    }
-
-    public static void increaseEmployeeCapacity(int newCapacity) {
-        if (newCapacity <= 100)
-            throw new IllegalStateException("New capacity cannot be <= 100");
-
-        EmployeeIDGenerator.changeIdLimit(newCapacity);
     }
 }
