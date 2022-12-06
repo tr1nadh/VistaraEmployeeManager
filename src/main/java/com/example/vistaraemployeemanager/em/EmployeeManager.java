3ad619ea2795e0eef8f6e65a4a02e944ab1f5b30
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-06 15:37:27 +0530

    Optimized view employees
    Stoped view employees quering when there is no change in db

--- src/main/java/com/example/vistaraemployeemanager/em/EmployeeManager.java
+++ src/main/java/com/example/vistaraemployeemanager/em/EmployeeManager.java
@@ -6,7 +6,11 @@ import com.example.vistaraemployeemanager.dao.EmployeeDao;
 
 public class EmployeeManager {
 
+    public static boolean hasChange = true;
+    public static ArrayList<Employee> empList;
+
     public static void add(Employee employee) {
+        hasChange = true;
         var response = EmployeeDao.add(employee);
         if (response > 0)
             System.out.println("Employee added: " + employee.getName());
@@ -15,6 +19,7 @@ public class EmployeeManager {
     }
 
     public static void remove(int employeeID) throws Exception {
+        hasChange = true;
         var response = EmployeeDao.removeAsync(employeeID);
         if (response.get() > 0)
             System.out.println("Employee with ID: " + employeeID + " is removed");
@@ -23,6 +28,7 @@ public class EmployeeManager {
     }
 
     public static void update(int employeeID, Employee employee) throws Exception {
+        hasChange = true;
         var response = EmployeeDao.updateAsync(employeeID, employee);
         if (response.get() > 0)
             System.out.println("Changes saved of ID: " + employeeID);
@@ -31,9 +37,11 @@ public class EmployeeManager {
     }
 
     public static ArrayList<Employee> getAllEmployees() throws Exception {
+        hasChange = false;
         var employees = EmployeeDao.getEmployeesAsync();
         System.out.println("Successfully retrieved employees");
-        return employees.get();
+        empList = employees.get();
+        return empList;
     }
 
     public static Optional<Employee> getEmployee(int employeeID) {
