c30be4aa58146573ccc87131fcd609d2d00f3e34
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-06 15:37:27 +0530

    Optimized view employees
    Stoped view employees quering when there is no change in db

--- src/main/java/com/example/vistaraemployeemanager/Servlets/View/ViewEmployeeServlet.java
+++ src/main/java/com/example/vistaraemployeemanager/Servlets/View/ViewEmployeeServlet.java
@@ -1,5 +1,6 @@
 package com.example.vistaraemployeemanager.Servlets.View;
 
+import com.example.vistaraemployeemanager.em.Employee;
 import com.example.vistaraemployeemanager.em.EmployeeManager;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -7,6 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 @WebServlet("/view")
@@ -15,11 +17,15 @@ public class ViewEmployeeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         try {
-            var empList = EmployeeManager.getAllEmployees();
+            var empList = getEmployees();
             req.setAttribute("empList", empList);
             req.getRequestDispatcher("view-employee.jsp").forward(req, res);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private ArrayList<Employee> getEmployees() throws Exception {
+        return (EmployeeManager.hasChange == true) ? EmployeeManager.getAllEmployees() : EmployeeManager.empList;
     }
 }
