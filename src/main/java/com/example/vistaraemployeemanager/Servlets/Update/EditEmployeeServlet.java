c2e3b5a675cbd262ca5bcf6cd4038b0cbdcad1da
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-06 14:25:01 +0530

    Edit employee changed to MVC pattern

--- src/main/java/com/example/vistaraemployeemanager/Servlets/Update/EditEmployeeServlet.java
+++ src/main/java/com/example/vistaraemployeemanager/Servlets/Update/EditEmployeeServlet.java
@@ -18,50 +18,10 @@ public class EditEmployeeServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        var writer = res.getWriter();
         var employee = getEmployeeDependsOnMethod(req);
         if (employee == null) throw new IllegalStateException("No user with this ID");
-        showFields(req, res, writer, employee);
-        writer.close();
-    }
-
-    private void showFields(HttpServletRequest req, HttpServletResponse res, PrintWriter writer, Employee employee) throws ServletException, IOException {
-        req.getRequestDispatcher("edit-employee.html").include(req, res);
-        var htmlData = "<div class='container border mt-3'>" +
-                "      <h2 class='text-center mb-4'>Edit employee</h2>" +
-                "<form action='saveEmployee' method='post'>\n" +
-                "  <label class='visually-hidden-focusable'>Id <input type='text' name='id' value='"+ employee.getId() +"'></label>\n" +
-                "\n" +
-                "<div class='form-floating mb-3 mt-3'>" +
-                "<input class='form-control' type='name' placeholder='Enter name' name='name' value='"+ employee.getName() +"' />" +
-                "  <label>Name</label>\n" +
-                "</div>" +
-                "\n" +
-                "<div class='form-floating mb-3 mt-3'>" +
-                "<input class='form-control' type='password' placeholder='Enter password' name='password' value='"+ employee.getPassword() +"' />" +
-                "  <label>Password</label>\n" +
-                "</div>" +
-                "\n" +
-                "<div class='form-floating mb-3 mt-3'>" +
-                "<input class='form-control' type='email' placeholder='Enter email' name='email' value='"+ employee.getEmail() +"' />" +
-                "  <label>Email</label>\n" +
-                "</div>"+
-                "\n" +
-                "  <div class='mb-3 mt-3'>\n" +
-                "    <select class='form-select form-select-md' name='country'>\n" +
-                "      <option value='India'>India</option>\n" +
-                "      <option value='UK'>UK</option>\n" +
-                "      <option value='USA'>USA</option>\n" +
-                "      <option value='UAE'>UAE</option>\n" +
-                "      <option value='Other'>Other</option>\n" +
-                "    </select>\n" +
-                "  </div>\n" +
-                "\n" +
-                "  <input class='btn btn-outline-success' type='submit' value='Edit & Save'>\n" +
-                " <a class='btn btn-danger' href='view'>Cancel</a>" +
-                "</form>" +
-                "</div>";
-        writer.println(htmlData);
+        req.setAttribute("emp", employee);
+        req.getRequestDispatcher("edit-employee.jsp").forward(req, res);
     }
 
     private Employee getEmployeeDependsOnMethod(HttpServletRequest req) {
