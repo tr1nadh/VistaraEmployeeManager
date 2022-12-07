73142cab6d594b0c352c18df667ae689b6cb57fd
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-07 16:59:37 +0530

    Refactor

--- src/main/java/com/example/vistaraemployeemanager/manager/EmployeeManager.java
+++ src/main/java/com/example/vistaraemployeemanager/manager/EmployeeManager.java
@@ -13,35 +13,36 @@ public class EmployeeManager {
 
     public static void add(Employee employee) {
         hasChange = true;
+
         var response = EmployeeDao.add(employee);
-        if (response > 0)
-            System.out.println("Employee added: " + employee.getName());
-        else
-            System.out.println("Unable to add employee: " + employee.getName());
+
+        if (response > 0) System.out.println("Employee added: " + employee.getName());
+        else System.out.println("Unable to add employee: " + employee.getName());
     }
 
     public static void remove(int employeeID) throws Exception {
         hasChange = true;
+
         var response = EmployeeDao.removeAsync(employeeID);
-        if (response.get() > 0)
-            System.out.println("Employee with ID: " + employeeID + " is removed");
-        else
-            System.out.println("Unable to remove employee with ID: " + employeeID);
+
+        if (response.get() > 0) System.out.println("Employee with ID: " + employeeID + " is removed");
+        else System.out.println("Unable to remove employee with ID: " + employeeID);
     }
 
     public static void update(int employeeID, Employee employee) throws Exception {
         hasChange = true;
+
         var response = EmployeeDao.updateAsync(employeeID, employee);
-        if (response.get() > 0)
-            System.out.println("Changes saved of ID: " + employeeID);
-        else
-            System.out.println("Unable to save changes of ID: " + employeeID);
+
+        if (response.get() > 0) System.out.println("Changes saved of ID: " + employeeID);
+        else System.out.println("Unable to save changes of ID: " + employeeID);
     }
 
     public static ArrayList<Employee> getAllEmployees() throws Exception {
         if (!hasChange) return empList;
         
         hasChange = false;
+
         empList = EmployeeDao.getEmployeesAsync().get();
         System.out.println("Successfully retrieved employees");
         
@@ -51,6 +52,7 @@ public class EmployeeManager {
     public static Optional<Employee> getEmployee(int employeeID) {
         var employee = EmployeeDao.getEmployee(employeeID);
         System.out.println("Successfully retrieved employee with id: " + employeeID);
+        
         return employee;
     }
 }
