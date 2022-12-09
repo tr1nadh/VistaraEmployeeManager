bd48b18063794fd9b0df63b1813692e80240b4cb
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-09 19:01:17 +0530

    Renamed & Changed static to obj methods

--- src/main/java/com/example/vistaraemployeemanager/database/JDBIManager.java
+++ src/main/java/com/example/vistaraemployeemanager/database/JDBIManager.java
@@ -2,12 +2,19 @@ package com.example.vistaraemployeemanager.database;
 
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.mapper.reflect.FieldMapper;
-
 import com.example.vistaraemployeemanager.model.Employee;
 
-public class Connector {
-    public static Jdbi getJdbiConnector() {
-        var jdbi = Jdbi.create(Datasource.getDatasource());
+
+public class JDBIManager {
+
+    private final IDatasource datasource;
+
+    public JDBIManager(IDatasource datasource) {
+        this.datasource = datasource;
+    }
+
+    public Jdbi getJdbiConnector() {
+        var jdbi = Jdbi.create(datasource.getDatasource());
         jdbi.registerRowMapper(FieldMapper.factory(Employee.class));
         return jdbi;
     }
