1ac18216764d991adbe73a360703cc8961a7e4aa
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-09 12:53:31 +0530

    Removed unnecessary filter

--- src/main/java/com/example/vistaraemployeemanager/controller/add/AddEmployeeServlet.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/add/AddEmployeeServlet.java
@@ -16,12 +16,22 @@ public class AddEmployeeServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        var employee = (Employee) req.getAttribute("employee");
+        var employee = getEmployee(req);
         
        EmployeeManager.add(employee);
 
         // TODO: Find a way to show success alert
         res.sendRedirect("add");
+    }
+
+    private Employee getEmployee(HttpServletRequest req) {
+        var employee = new Employee();
+        employee.setName(req.getParameter("name"));
+        employee.setPassword(req.getParameter("password"));
+        employee.setEmail(req.getParameter("email"));
+        employee.setCountry(req.getParameter("country"));
+
+        return employee;
     }
 
 }
