fc6a80f8c65e25a85c386fca47de62289a9bd1d9
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-15 10:14:50 +0530

    Added persistence annotations

--- src/main/java/com/example/vistaraemployeemanager/model/Employee.java
+++ src/main/java/com/example/vistaraemployeemanager/model/Employee.java
@@ -1,8 +1,22 @@
 package com.example.vistaraemployeemanager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String name, password, email, country;
+    private String name;
+    private String password;
+    private String email;
+    private String country;
 
     public int getId() {
         return id;
