5e11f3796df48fe8d1cc9cbfff9186092acb09af
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2022-12-17 20:19:23 +0530

    Empty

--- src/main/java/com/example/vistaraemployeemanager/Main.java
+++ src/main/java/com/example/vistaraemployeemanager/Main.java
@@ -1,7 +1,5 @@
 package com.example.vistaraemployeemanager;
-
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
     }
 }
