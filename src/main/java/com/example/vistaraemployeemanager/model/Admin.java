c39e56ad6b2f61b554affa15b80d73f571319930
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-01-25 17:14:19 +0530

    innitial commit

---
+++ src/main/java/com/example/vistaraemployeemanager/model/Admin.java
@@ -0,0 +1,19 @@
+package com.example.vistaraemployeemanager.model;
+
+public class Admin {
+    private String name;
+    private String password;
+
+    public Admin(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+}
