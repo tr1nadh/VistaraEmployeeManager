f9b215e1f16ab002434ad2ec8e7d38e74397d403
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-01-25 17:14:02 +0530

    added functionality

--- src/main/java/com/example/vistaraemployeemanager/controller/login/LoginAdminServlet.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/login/LoginAdminServlet.java
@@ -1,8 +1,32 @@
 package com.example.vistaraemployeemanager.controller.login;
 
-import com.example.vistaraemployeemanager.controller.ControllerHelper;
+import java.io.IOException;
 
+import com.example.vistaraemployeemanager.controller.ControllerHelper;
+import com.example.vistaraemployeemanager.model.Admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginAdmin")
 public class LoginAdminServlet extends ControllerHelper {
     
-    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        var admin = getAdmin(req);
+
+        if (admin.getName().equals("admin") && admin.getPassword().equals("pass")) {
+            req.getRequestDispatcher("view").forward(req, res);
+        }
+
+        req.setAttribute("alrtMsg", "Name or password is wrong");
+        req.setAttribute("forwardAddr", "login");
+        req.getRequestDispatcher("alert-n-forward.jsp").forward(req, res);
+    }
+
+    private Admin getAdmin(HttpServletRequest req) {
+        return new Admin(req.getParameter("name"), req.getParameter("password"));
+    }
 }
