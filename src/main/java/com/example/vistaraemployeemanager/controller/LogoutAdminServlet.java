16d799be3f7c9c15006fc20893e491bae47a7ba5
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-01-26 19:38:56 +0530

    Added logout functionality

---
+++ src/main/java/com/example/vistaraemployeemanager/controller/LogoutAdminServlet.java
@@ -0,0 +1,20 @@
+package com.example.vistaraemployeemanager.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class LogoutAdminServlet extends ControllerHelper {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.getSession().invalidate();
+        res.sendRedirect("login");
+    }
+    
+    
+}
