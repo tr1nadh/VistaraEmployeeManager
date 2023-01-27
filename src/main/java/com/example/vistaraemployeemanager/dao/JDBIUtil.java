22010567c0828467b7dcf665bd1618aeed3b1638
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-01-27 18:55:05 +0530

    Util class for JDBI

---
+++ src/main/java/com/example/vistaraemployeemanager/dao/JDBIUtil.java
@@ -0,0 +1,10 @@
+package com.example.vistaraemployeemanager.dao;
+
+import org.jdbi.v3.core.Jdbi;
+
+public class JDBIUtil {
+    
+    public static Jdbi getJDBI() {
+        return Jdbi.create("jdbc:mysql://localhost:3306/vemdb", "root", "***REMOVED***"); 
+    }
+}
