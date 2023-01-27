0e26b918d969e162c7b16e9a72d3b85a18b67d6f
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-01-27 18:54:36 +0530

    Added session manager method

--- src/main/java/com/example/vistaraemployeemanager/controller/ControllerHelper.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/ControllerHelper.java
@@ -1,10 +1,16 @@
 package com.example.vistaraemployeemanager.controller;
 
 import com.example.vistaraemployeemanager.manager.EmployeeManager;
+import com.example.vistaraemployeemanager.manager.SessionManager;
 
 import jakarta.servlet.http.HttpServlet;
 
 public abstract class ControllerHelper extends HttpServlet {
+    
+    protected SessionManager getSessionManager() {
+        return (SessionManager) getServletContext().getAttribute("sessionManager");
+    }
+
     protected EmployeeManager getEmployeeManager() {
         return (EmployeeManager) getServletContext().getAttribute("employeeManager");
     }
