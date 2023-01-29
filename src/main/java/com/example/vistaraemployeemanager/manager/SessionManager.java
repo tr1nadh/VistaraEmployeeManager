b4cd4b9027a8f9aed3ec5cb617ca786d28e07046
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-01-29 21:49:45 +0530

    Removed, Refactored & Added methods
    Removed contains session method,
    Refactored methods names,
    Added HashMap for managing sessions & Added set/get methods for login status

--- src/main/java/com/example/vistaraemployeemanager/manager/SessionManager.java
+++ src/main/java/com/example/vistaraemployeemanager/manager/SessionManager.java
@@ -1,31 +1,32 @@
 package com.example.vistaraemployeemanager.manager;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import com.example.vistaraemployeemanager.dao.SessionDao;
 
 public class SessionManager {
 
-    private List<String> currentSessionIDs = new ArrayList<>();
+    private HashMap<String, Boolean> sessions = new HashMap<>();
     
-    public void saveSessionID(String sessionID) {
+    public void saveSession(String sessionID) {
+        sessions.putIfAbsent(sessionID, false);
         SessionDao.saveSessionID(sessionID);
     }
 
-    public boolean containsSessionID(String sessionID) {
-        if (currentSessionIDs.contains(sessionID)) return true;
-
-        var storedID = SessionDao.getSessionID(sessionID);
-        if (storedID.isPresent() && storedID.get().equals(sessionID)) {
-            currentSessionIDs.add(sessionID);
-            return true;
-        };
-
-        return false;
+    public void removeSession(String sessionID) {
+        sessions.remove(sessionID);
+        SessionDao.removeSession(sessionID);
     }
 
-    public void removesessionID(String sessionID) {
-        currentSessionIDs.remove(sessionID);
-        SessionDao.removeSessionID(sessionID);
+    public void setLoginStatus(String sessionID, boolean status) {
+        sessions.put(sessionID, status);
+        SessionDao.setLoginStatus(sessionID, status);
+    }
+
+    public boolean getLoginStatus(String sessionID) {
+        if (sessions.containsKey(sessionID)) return sessions.get(sessionID);
+        
+        var status = SessionDao.getLoginStatus(sessionID);
+        sessions.put(sessionID, status);
+        return status;
     }
 }
