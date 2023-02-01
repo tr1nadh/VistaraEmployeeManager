e7b51e7cdeb54f3d767838ca288dca06fbe66724
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-01 16:08:28 +0530

    Added environment variable

--- src/main/java/com/example/vistaraemployeemanager/dao/JDBIUtil.java
+++ src/main/java/com/example/vistaraemployeemanager/dao/JDBIUtil.java
@@ -5,6 +5,6 @@ import org.jdbi.v3.core.Jdbi;
 public class JDBIUtil {
     
     public static Jdbi getJDBI() {
-        return Jdbi.create("jdbc:mysql://localhost:3306/vemdb", "root", "pass"); 
+        return Jdbi.create("jdbc:mysql://localhost:3306/vemdb", "root", System.getenv("DB_PASSWORD")); 
     }
 }
