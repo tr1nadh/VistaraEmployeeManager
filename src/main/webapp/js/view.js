5de33a747457eab26571551385f2f2f749c2a08f
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-02 12:09:56 +0530

    Created view javascirpt file for view-employee.jsp

---
+++ src/main/webapp/js/view.js
@@ -0,0 +1,20 @@
+function onLoadFunction(isEmpListEmpty, nextPageValue) {
+    if (isEmpListEmpty) {
+        document.getElementById("next-item").classList.add("disabled");
+    }
+
+    if (nextPageValue == 3) {
+        document.getElementById("prev-link").href = "view";
+    }
+    if (nextPageValue <= 2) {
+        document.getElementById("prev-item").classList.add("disabled");
+        document.getElementById("first-item").classList.add("disabled");
+    }
+}
+
+function confirmDelete(id) {
+    let text = "Are you sure, you want to delete?";
+    if (confirm(text) == true) {
+        window.location.assign("deleteEmployee?id=" + id);
+    }
+}
