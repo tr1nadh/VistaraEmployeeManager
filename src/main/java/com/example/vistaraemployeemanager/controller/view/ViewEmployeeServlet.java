e09980ab5ab70d69dd9698436a78dd1e9050dead
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-02 16:35:44 +0530

    Refactored the code

--- src/main/java/com/example/vistaraemployeemanager/controller/view/ViewEmployeeServlet.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/view/ViewEmployeeServlet.java
@@ -18,22 +18,28 @@ public class ViewEmployeeServlet extends ControllerHelper {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        var max = 15; 
+        var max = 10; 
 
-        var pageStr = req.getParameter("p");
-        if (pageStr == null) pageStr = "0";
-        var pageInt = Integer.parseInt(pageStr) - 1;
-        pageInt = (pageInt < 0) ? 0 : pageInt;
-        var startFrom = max * pageInt;
-        pageInt++;
-        var next = pageInt + 1;
-        var prev = pageInt - 1;
-
+        var pageStr = getPageValue(req);
+        var pageInt = Integer.parseInt(pageStr);
+        var startFrom = calculateStartFromValue(pageInt - 1, max);
         var empList = getEmployeeManager().getEmployees(startFrom, max);
-        if (empList.isEmpty()) --next;
+        var next = (empList.isEmpty()) ? pageInt : (pageInt + 1);
+        var prev = pageInt - 1;
         req.setAttribute("empList", empList);
         req.setAttribute("next", next);
         req.setAttribute("prev", prev);
+
         req.getRequestDispatcher("view-employee.jsp").forward(req, res);
+    }
+
+    private int calculateStartFromValue(int pageInt, int max) {
+        pageInt = (pageInt < 0) ? 0 : pageInt;
+        return pageInt * max;
+    }
+
+    private String getPageValue(HttpServletRequest req) {
+        var pageStr = req.getParameter("p");
+        return (pageStr == null) ? "1" : pageStr;
     }
 }
