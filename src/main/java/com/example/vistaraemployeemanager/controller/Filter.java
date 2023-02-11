78a27dad577df68a879ebc630b44164f04b35036
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-11 11:54:35 +0530

    Added common methods

--- src/main/java/com/example/vistaraemployeemanager/controller/Filter.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/Filter.java
@@ -1,5 +1,16 @@
 package com.example.vistaraemployeemanager.controller;
 
+import com.example.vistaraemployeemanager.manager.EmployeeManager;
+import com.example.vistaraemployeemanager.manager.SessionManager;
+import jakarta.servlet.ServletRequest;
+
 public abstract class Filter implements jakarta.servlet.Filter {
+
+    protected SessionManager getSessionManager(ServletRequest request) {
+        return (SessionManager) request.getServletContext().getAttribute("sessionManager");
+    }
     
+    protected EmployeeManager getEmployeeManager(ServletRequest request) {
+        return (EmployeeManager) request.getServletContext().getAttribute("employeeManager");
+    }
 }
