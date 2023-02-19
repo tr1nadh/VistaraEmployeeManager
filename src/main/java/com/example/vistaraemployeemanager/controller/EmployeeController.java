a86d85d12cca54ee03ab33eee3b3e478ce8e3465
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-19 18:58:20 +0530

    Added delete method

--- src/main/java/com/example/vistaraemployeemanager/controller/EmployeeController.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/EmployeeController.java
@@ -2,8 +2,10 @@ package com.example.vistaraemployeemanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.vistaraemployeemanager.manager.EmployeeManager;
 import com.example.vistaraemployeemanager.model.Employee;
@@ -28,5 +30,12 @@ public class EmployeeController {
     @RequestMapping("/add")
     public String showAdd() {
         return "add-employee.jsp";
+    }
+
+    @GetMapping("/deleteEmployee")
+    public String delete(@RequestParam int id) throws Exception {
+        manager.remove(id);
+
+        return "/view";
     }
 }
