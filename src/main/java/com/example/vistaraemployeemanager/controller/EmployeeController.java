1edea6832e79742ebf6d47a4955626de0d5eac0f
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-19 18:41:59 +0530

    Autowired the dependency

--- src/main/java/com/example/vistaraemployeemanager/controller/EmployeeController.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/EmployeeController.java
@@ -1,12 +1,13 @@
 package com.example.vistaraemployeemanager.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import com.example.vistaraemployeemanager.manager.EmployeeManager;
 
 @Controller
 public class EmployeeController {
     
+    @Autowired
     private final EmployeeManager manager;
 
     public EmployeeController(EmployeeManager manager) {
