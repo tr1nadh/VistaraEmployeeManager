f8f1779024a7dbb722d6652907de931ed6485609
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-20 11:41:50 +0530

    Added login admin method

--- src/main/java/com/example/vistaraemployeemanager/controller/SessionController.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/SessionController.java
@@ -1,8 +1,23 @@
 package com.example.vistaraemployeemanager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.example.vistaraemployeemanager.model.Admin;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class SessionController {
     
+    @PostMapping("/loginAdmin")
+    public RedirectView loginAdmin(Admin admin, HttpServletRequest req) {
+        if (admin.getName().equals("admin") && admin.getPassword().equals("pass")) {
+            req.getSession().setAttribute("login_status", true);
+            return new RedirectView("view");
+        }
+
+        return new RedirectView("login");
+    }
 }
