b6a398f29b0762437296d31d51fbedc3b7c983a7
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-20 14:41:47 +0530

    Added prehandle method

--- src/main/java/com/example/vistaraemployeemanager/controller/ReqProcessController.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/ReqProcessController.java
@@ -1,8 +1,29 @@
 package com.example.vistaraemployeemanager.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerInterceptor;
+import com.example.vistaraemployeemanager.manager.SessionManager;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@Controller
-public class ReqProcessController {
-    
+@RequestMapping("/")
+public class ReqProcessController implements HandlerInterceptor {
+
+    @Autowired
+    private final SessionManager manager;
+
+    public ReqProcessController(SessionManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
+        var sessionId = req.getSession().getId();
+        if (!req.getRequestURI().equals("/app/loginAdmin") && manager.getLoginStatus(sessionId) == false) {
+            return false;
+        }
+
+        return true;
+    }
 }
