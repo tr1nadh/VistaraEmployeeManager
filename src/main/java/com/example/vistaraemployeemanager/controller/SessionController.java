7858fb8a02a90789f07dd84d5f516201c2f8cb1d
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-20 12:39:38 +0530

    Added show login method and dependency

--- src/main/java/com/example/vistaraemployeemanager/controller/SessionController.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/SessionController.java
@@ -1,16 +1,27 @@
 package com.example.vistaraemployeemanager.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.example.vistaraemployeemanager.manager.SessionManager;
 import com.example.vistaraemployeemanager.model.Admin;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class SessionController {
+
+    @Autowired
+    private final SessionManager manager;
+
+    public SessionController(SessionManager manager) {
+        this.manager = manager;
+    }
     
     @PostMapping("/loginAdmin")
     public RedirectView loginAdmin(Admin admin, HttpServletRequest req) {
@@ -27,5 +38,14 @@ public class SessionController {
         req.getSession().setAttribute("login_status", false);
         
         return new RedirectView("login");
+    }
+
+    @RequestMapping("/login")
+    public ModelAndView showLogin(HttpServletRequest req) {
+        if (manager.getLoginStatus(req.getSession().getId())) {
+            return new ModelAndView("redirect:/view");
+        }
+
+        return new ModelAndView("login-admin.jsp");
     }
 }
