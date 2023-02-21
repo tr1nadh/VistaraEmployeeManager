59936f91ab415b6e96c99f590a3dd3bd82b219e5
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-21 10:31:46 +0530

    Added resource handlers

--- src/main/java/com/example/vistaraemployeemanager/config/AppConfig.java
+++ src/main/java/com/example/vistaraemployeemanager/config/AppConfig.java
@@ -2,9 +2,18 @@ package com.example.vistaraemployeemanager.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan({"com.example.vistaraemployeemanager.manager", "com.example.vistaraemployeemanager.dao", "com.example.vistaraemployeemanager.controller"})
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurationSupport {
     
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
 }
