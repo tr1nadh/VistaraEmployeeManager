a51f7219856814a470b12b02759ccf380ceaf178
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-21 15:02:28 +0530

    Removed sessionManager & EmployeeManager context attributes

--- src/main/java/com/example/vistaraemployeemanager/listener/AppContextListener.java
+++ src/main/java/com/example/vistaraemployeemanager/listener/AppContextListener.java
@@ -1,8 +1,5 @@
 package com.example.vistaraemployeemanager.listener;
 
-import com.example.vistaraemployeemanager.manager.EmployeeManager;
-import com.example.vistaraemployeemanager.manager.SessionManager;
-import com.example.vistaraemployeemanager.dao.EmployeeDao;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
@@ -13,11 +10,5 @@ public class AppContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
         var ctx = event.getServletContext();
         ctx.getSessionCookieConfig().setMaxAge(24 * 60 * 60);
-
-        var employeeManager = new EmployeeManager(new EmployeeDao());
-        var sessionManager = new SessionManager();
-
-        ctx.setAttribute("employeeManager", employeeManager);
-        ctx.setAttribute("sessionManager", sessionManager);
     }
 }
