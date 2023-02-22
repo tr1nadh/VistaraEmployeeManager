4fa12f19e0ddf83f2d422969df4aadfeacdbbf73
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-22 16:11:28 +0530

    Added dependency instead

--- src/main/java/com/example/vistaraemployeemanager/service/SessionService.java
+++ src/main/java/com/example/vistaraemployeemanager/service/SessionService.java
@@ -10,26 +10,32 @@ import com.example.vistaraemployeemanager.dao.SessionDao;
 public class SessionService {
 
     private HashMap<String, Boolean> sessions = new HashMap<>();
+
+    private final SessionDao sessionDao;
+
+    public SessionService(SessionDao sessionDao) {
+        this.sessionDao = sessionDao;
+    }
     
     public void saveSession(String sessionID) {
         sessions.putIfAbsent(sessionID, false);
-        SessionDao.saveSessionID(sessionID);
+        sessionDao.saveSessionID(sessionID);
     }
 
     public void removeSession(String sessionID) {
         sessions.remove(sessionID);
-        SessionDao.removeSession(sessionID);
+        sessionDao.removeSession(sessionID);
     }
 
     public void setLoginStatus(String sessionID, boolean status) {
         sessions.put(sessionID, status);
-        SessionDao.setLoginStatus(sessionID, status);
+        sessionDao.setLoginStatus(sessionID, status);
     }
 
     public boolean getLoginStatus(String sessionID) {
         if (sessions.containsKey(sessionID)) return sessions.get(sessionID);
         
-        var status = SessionDao.getLoginStatus(sessionID);
+        var status = sessionDao.getLoginStatus(sessionID);
         sessions.put(sessionID, status);
         return status;
     }
