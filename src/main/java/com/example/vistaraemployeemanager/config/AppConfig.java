4e51fce3d64739db190968687f08d36bb8c23c8f
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-23 19:05:59 +0530

    Removed deprecated implimentation & Changed the mapping

--- src/main/java/com/example/vistaraemployeemanager/config/AppConfig.java
+++ src/main/java/com/example/vistaraemployeemanager/config/AppConfig.java
@@ -4,16 +4,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan({"com.example.vistaraemployeemanager.service", "com.example.vistaraemployeemanager.dao", "com.example.vistaraemployeemanager.controller"})
-public class AppConfig extends WebMvcConfigurationSupport {
+public class AppConfig implements WebMvcConfigurer {
     
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
