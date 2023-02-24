29755ed150979db7644b73b8c63189bd0997ece2
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-24 20:21:09 +0530

    Added aspectJautoproxy annotation & refactored the component scan locations

--- src/main/java/com/example/vistaraemployeemanager/config/AppConfig.java
+++ src/main/java/com/example/vistaraemployeemanager/config/AppConfig.java
@@ -2,13 +2,15 @@ package com.example.vistaraemployeemanager.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan({"com.example.vistaraemployeemanager.service", "com.example.vistaraemployeemanager.dao", "com.example.vistaraemployeemanager.controller"})
+@EnableAspectJAutoProxy
+@ComponentScan({"com.example.vistaraemployeemanager"})
 public class AppConfig implements WebMvcConfigurer {
     
     @Override
