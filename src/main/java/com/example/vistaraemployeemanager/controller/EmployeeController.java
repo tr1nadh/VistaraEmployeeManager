c8a3b61e6c1cc86267cf7acc2fbc6c7904b820a3
Cybercryptic <53462309+cybercryptic@users.noreply.github.com>
2023-02-25 12:28:29 +0530

    Refactored

--- src/main/java/com/example/vistaraemployeemanager/controller/EmployeeController.java
+++ src/main/java/com/example/vistaraemployeemanager/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
